9a34ec151789a56e475224eb23e040f8a4ea86b4
Gal Dagan <GalD@MININT-JHS4BS3.analyst.co.il>
2022-03-24 22:55:36 +0200

    Eslint and prettier configuration

--- Summary-Explain-And-Commands/0-Basic.js
+++ Summary-Explain-And-Commands/0-Basic.js
@@ -1,13 +1,17 @@
+//##################### yarn add -D
+// https://yarnpkg.com/cli/add
+
+//### Helpful project for typescript and basic sequelize
 // https://www.youtube.com/watch?v=yFgrSJGNj0E&ab_channel=DailyWebCoding
 
 // npm init -y
 // npx tsc --init
 
+//mkdir src | src/server.ts
+
 // npm i express morgan sequelize sqlite3 uuid express-validator bcrypt
 // npm i -D typescript @types/express ts-node-dev
 // npm i -D @types/bcrypt @types/morgan @types/uuid @types/validator
-
-//mkdir src | src/server.ts
 
 // "scripts": {
 //     "dev": "ts-node-dev --respawn src/server.ts",
